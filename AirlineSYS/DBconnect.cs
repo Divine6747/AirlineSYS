847a1b97a1f5c726a7e2cde26006897404c3b824
Divine Okhaus <Divine.Okhaus@students.ittralee.ie>
2024-02-06 09:20:14 +0000

    From T302 09:19

---
+++ AirlineSYS/DBconnect.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirlineSYS
+{
+    class DBConnect
+    {
+        //public const String oradb = "Data Source = localhost/orcl; User Id = C##User1; Password = 123456;";
+
+        public const String oradb = "Data Source = studentoracle:1521/orcl; User Id = T00233163; Password = xbkg*wfU6km9;";
+        //public const String oradb = "Data Source = studentoracle/orcl; User Id = Leccw; Password = 123456;";
+
+    }
+}
