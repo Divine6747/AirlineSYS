5e189b87eab8ae0eead6859c280b112982d101d7
Divine6747 <Divine.Okhaus@students.ittralee.ie>
2024-02-08 12:53:55 +0000

    Room: S302 Date: 08/02/24 Time: 12:53

--- AirlineSYS/Route.cs
+++ AirlineSYS/Route.cs
@@ -50,43 +50,89 @@ namespace AirlineSYS
             Email = email;
             Status = status;
         }
+
+        //These are the getters
     
         public String getAirportCode() { return this.AirportCode;}
+
         public String getName() { return this.Name;}
+
         public String getStreet() { return this.Street;}
+
         public String getCity() { return this.City;}
+
         public String getCountry() { return this.Country;}
+
         public String getEircode() {  return this.Eircode;}
+
         public String getAddress() { return this.Address;}
-        public int getPhone() { return this.Phone;} 
+
+        public int getPhone() { return this.Phone;}
+        
         public String getEmail() { return this.Email;}
+
         public String getStatus() { return this.Status;}
 
-        /*
-        private String AirportCode;
-        private String Name;
-        private String Street;
-        private String City;
-        private String Country;
-        private String Eircode;
-        private String Address;
-        private int Phone;
-        private String Email;
-        private String Status;
-     */
-        public void setAirportCode(String AirportCode) { this.AirportCode = airportCode;}
-        public void setName(String Name) { Name = Name;}
-        public void setStreet(String Street) { Street = Street; }
-        public void setCity(String City) { City = City; }
-        public void setCountry(String Country) { Country = Country; }
-        public void setEircode(String Eircode) { Eircode = Eircode; }
-        public void setAddress(String Address) { Address = Address; }
-        public void setPhone(int Phone) { Phone = Phone; }
-        public void setEmail(String Email) { Email = Email; }
-        public void setStatus(String Status) { Status = Status; }
 
-        
+        //These are the setters
+        public void setAirportCode(String AirportCode) { this.AirportCode = airportCode;}
+
+        public void setName(String Name) { this.Name = Name;}
+
+        public void setStreet(String Street) { this.Street = Street; }
+
+        public void setCity(String City) { this.City = City; }
+
+        public void setCountry(String Country) { this.Country = Country; }
+
+        public void setEircode(String Eircode) { this.Eircode = Eircode; }
+
+        public void setAddress(String Address) { this.Address = Address; }
+
+        public void setPhone(int Phone) { this.Phone = Phone; }
+
+        public void setEmail(String Email) { this.Email = Email; }
+
+        public void setStatus(String Status) { this.Status = Status; }
+
 
     
+    }
+    class Operators
+    {
+        private String OperatorCode;
+        private String Name;
+        private String City;
+        private String Country;
+        private String Status;
+
+        public Operators()
+        {
+            this.OperatorCode = "";
+            this.Name = "";
+            this.City = "";
+            this.Country = "";
+            this.Status = "";
+        }
+
+        public Operators(String operatorCode, String name, String city, String country, String status)
+        {
+            OperatorCode = operatorCode;
+            Name = name;
+            City = city;
+            Country = country;
+            Status = status;
+        }
+
+        public String getOperatorCode() {  return OperatorCode; }
+
+        public String getName() { return Name; }
+
+        public String getCity() { return City; }
+
+        public String getCountry() { return Country; }
+
+        public String getStatus() { return Status; }
+
     }
 }
