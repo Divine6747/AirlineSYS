ab2ea264f56a85e41819390395539290a0400819
Divine6747 <Divine.Okhaus@students.ittralee.ie>
2024-02-08 11:08:17 +0000

    t302 11:08

---
+++ AirlineSYS/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AirlineSYS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            String line;
+            try
+            {
+                //Pass the file path and file name to the StreamReader constructor
+                StreamReader sr = new StreamReader("C:\\Users\\T00233163\\Documents\\Y2_project\\credentials.txt");
+                //Read the first line of text
+                line = sr.ReadLine();
+
+                String[] creds = line.Split(',');
+                String username = creds[0];
+                String password = creds[1];
+
+                //close the file
+                sr.Close();
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Executing finally block.");
+            }
+            InitializeComponent();
+        }
+    }
+}
---
+++ AirlineSYS/Route.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirlineSYS
+{
+    class Airport
+    {
+        private String AirportCode;
+        private String Name;
+        private String Street;
+        private String City;
+        private String Country;
+        private String Eircode;
+        private String Address;
+        private int Phone;
+        private String Email;
+        private String Status;
+
+        
+
+
+        public Airport()
+        {
+            this.AirportCode = "";
+            this.Name = "";
+            this.Street = "";
+            this.City = "";
+            this.Country = "";
+            this.Eircode = "";
+            this.Address = "";
+            this.Phone = 0;
+            this.Email = "";
+            this.Status = "";
+        }
+
+        public Airport(string airportCode, string name, string street, string city, string country, string eircode, string address, int phone, string email, string status)
+        {
+            AirportCode = airportCode;
+            Name = name;
+            Street = street;
+            City = city;
+            Country = country;
+            Eircode = eircode;
+            Address = address;
+            Phone = phone;
+            Email = email;
+            Status = status;
+        }
+    
+        public String getAirportCode() { return this.AirportCode;}
+        public String getName() { return this.Name;}
+        public String getStreet() { return this.Street;}
+        public String getCity() { return this.City;}
+        public String getCountry() { return this.Country;}
+        public String getEircode() {  return this.Eircode;}
+        public String getAddress() { return this.Address;}
+        public int getPhone() { return this.Phone;} 
+        public String getEmail() { return this.Email;}
+        public String getStatus() { return this.Status;}
+
+        /*
+        private String AirportCode;
+        private String Name;
+        private String Street;
+        private String City;
+        private String Country;
+        private String Eircode;
+        private String Address;
+        private int Phone;
+        private String Email;
+        private String Status;
+     */
+        public void setAirportCode(String AirportCode) { this.AirportCode = airportCode;}
+        public void setName(String Name) { Name = Name;}
+        public void setStreet(String Street) { Street = Street; }
+        public void setCity(String City) { City = City; }
+        public void setCountry(String Country) { Country = Country; }
+        public void setEircode(String Eircode) { Eircode = Eircode; }
+        public void setAddress(String Address) { Address = Address; }
+        public void setPhone(int Phone) { Phone = Phone; }
+        public void setEmail(String Email) { Email = Email; }
+        public void setStatus(String Status) { Status = Status; }
+
+        
+
+    
+    }
+}
