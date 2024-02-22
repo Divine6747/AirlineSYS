1dea1801eb4e8272d9e5294ef0fde6a419922f30
Divine6747 <Divine.Okhaus@students.ittralee.ie>
2024-02-22 17:59:31 +0000

    Still thinking the logic for the getFlightNumber.

--- AirlineSYS/Flight.cs
+++ AirlineSYS/Flight.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Data;
 using System.Text;
-using System.Threading.Tasks;
+using System.Windows.Forms;
+using Oracle.ManagedDataAccess.Client;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace AirlineSYS
 {
@@ -13,46 +15,84 @@ namespace AirlineSYS
         private DateTime FlightDate;
         private DateTime FlightTime;
         private string EstArrTime;
-        private int numSeats;
-        private int numSeatAvail;
+        private int NumSeats;
+        private int NumSeatAvail;
         private string Status;
 
 
-        public string getFlightNumber() { return this.FlightNumber; }
-        public int getRouteID() { return this.RouteID; }
-        public DateTime getFlightdate() { return this.FlightDate; }
-        public int getEstArrTime() { return this.EstArrTime; }
-        public int getNumSeats() {  return this.numSeats; }
-        public int getNumSeatAvail() { return this.numSeatAvail; }
-        public string getStatus() { return this.Status; }
-
-
-
-        public Flight() {
-
-            this.FlightNumber = "";
-            this.RouteID = 0;
-            this.FlightDate = "";
-            this.FlightTime = "";
-            this.EstArrTime = "";
-            this.numSeats = 0;
-            this.numSeatAvail = 0;
-            this.Status = "";
-
-        }
-
-        public Flight(string flightNumber, int routeID, string flightDate, string flightTime, string estArrTime, int numSeats, int numSeatAvail, string status)
+        public Flight(string flightNumber, int routeID, DateTime flightDate, DateTime flightTime, string estArrTime, int numSeats, int numSeatAvail, string status)
         {
-            FlightNumber = flightNumber;
-            RouteID = routeID;
-            FlightDate = flightDate;
-            FlightTime = flightTime;
-            EstArrTime = estArrTime;
-            this.numSeats = numSeats;
-            this.numSeatAvail = numSeatAvail;
-            Status = status;
+            FlightNumber = "";
+            RouteID = 0;
+            FlightDate = DateTime.Now;
+            FlightTime = DateTime.Now;
+            EstArrTime = "";
+            NumSeats = 0;
+            NumSeatAvail = 0;
+            Status = "";
         }
+
+
+
+        public Flight(string flightNumber, int routeID, DateTime flightDate, DateTime flightTime, string estArrTime, int numSeats, int numSeatAvail, string status)
+        {
+            this.FlightNumber = flightNumber;
+            this.RouteID = routeID;
+            this.FlightDate = flightDate;
+            this.FlightTime = flightTime;
+            this.EstArrTime = estArrTime;
+            this.NumSeats = numSeats;
+            this.NumSeatAvail = numSeatAvail;
+            this.Status = status;
+        }
+
+
+        //Getter
+        public string getFlightNumber() { return FlightNumber; }
+
+        public int getRouteID() {  return RouteID; }
+
+        public DateTime getFlightDate() { return FlightDate; }
+
+        public DateTime getFlightTime() {  return FlightTime; }
+
+        public int getNumSeats() {  return NumSeats; }
+
+        public int getNumSeatAvail() {  return NumSeatAvail; }
+
+        public string getStatus() { return Status; }
+
+
+
+        //Setters
+        public void setFlightNumber(string flightNumber) {  Status = flightNumber; }
+
+        public void setRouteID(int routeID) {  RouteID = routeID; }
+
+        public void setFlightDate(DateTime flightDate) { FlightDate = flightDate; }
+
+        public void setFlightTime(DateTime flightTime) { FlightTime = flightTime; }
+
+        public void setEstArrTime(DateTime estArrTime) {  EstArrTime = estArrTime; }
+
+        public void setNumseats(int numSeats) {  NumSeats = numSeats; }
+
+        public void setNumSeatsAvails(int numSeatsAvails) { NumSeatAvail = numSeatsAvails;}
+
+        public void setStatus(string status) { Status = status; }
+
+
+        public static string getFlightNumber() 
+        {
+
+        }
+
+
+
+
+
 
 
     }
+
 }
